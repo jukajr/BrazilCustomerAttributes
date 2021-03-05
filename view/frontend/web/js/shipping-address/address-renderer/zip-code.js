9732ed35c147fd61f45917cbc8fc7b74a712291c
jukajr <carlosazevedojr@gmail.com>
2021-03-04 22:57:49 -0300

    Update zip-code.js

--- view/frontend/web/js/shipping-address/address-renderer/zip-code.js
+++ view/frontend/web/js/shipping-address/address-renderer/zip-code.js
@@ -5,9 +5,10 @@ define([
     'Magento_Ui/js/form/element/abstract',
     'jquery',
     'inputMask',
-    'mage/url',
+    'mage/url',    
+    'Magento_Checkout/js/action/get-payment-information',
     'loader'
-], function (_, ko, registry, Abstract, jquery, mask, url) {
+], function (_, ko, registry, Abstract, jquery, mask, url, getPaymentInformationAction) {
     'use strict';
 
     return Abstract.extend({
@@ -66,10 +67,9 @@ define([
                 var value = this.value();
                 value = value.replace('-', '');
 
-                var ajaxurl = url.build('/rest/V1/magedev-brazil-zipcode/search/' + value);
 
                 jquery.ajax({
-                    url: ajaxurl,
+                    url: `https://viacep.com.br/ws/${value}/json/`,
                     dataType: 'json',
                     timeout: 4000
                 }).done(function (data) {
@@ -77,23 +77,114 @@ define([
                         // TODO
                     }else{
                         if(registry.get(element.parentName + '.' + 'street.0')){
-                            registry.get(element.parentName + '.' + 'street.0').value(data.street ?? '');
+                            registry.get(element.parentName + '.' + 'street.0').value(data.logradouro ?? '');
                         }
                         if(registry.get(element.parentName + '.' + 'street.2')){
-                            registry.get(element.parentName + '.' + 'street.2').value(data.neighborhood ?? '');
-                        }
-                        if(registry.get(element.parentName + '.' + 'street.3')){
-                            registry.get(element.parentName + '.' + 'street.3').value(data.additional_info ?? '');
+                            registry.get(element.parentName + '.' + 'street.2').value(data.bairro ?? '');
                         }
                         if(registry.get(element.parentName + '.' + 'city')){
-                            registry.get(element.parentName + '.' + 'city').value(data.city ?? '');
+                            registry.get(element.parentName + '.' + 'city').value(data.localidade ?? '');
                         }
+
+                        var slcUF = "";
+
+                        switch(data.uf) {
+                            case "AC":
+                                slcUF = "485";
+                                break;
+                            case "AL":
+                                slcUF = "486";
+                                break;
+                            case "AP":
+                                slcUF = "487";
+                                break;
+                            case "AM":
+                                slcUF = "488";
+                                break;
+                            case "BA":
+                                slcUF = "489";
+                                break;
+                            case "CE":
+                                slcUF = "490";
+                                break;
+                            case "ES":
+                                slcUF = "491";
+                                break;
+                            case "GO":
+                                slcUF = "492";
+                                break;
+                            case "MA":
+                                slcUF = "493";
+                                break;
+                            case "MT":
+                                slcUF = "494";
+                                break;
+                            case "MS":
+                                slcUF = "495";
+                                break;
+                            case "MG":
+                                slcUF = "496";
+                                break;
+                            case "PA":
+                                slcUF = "497";
+                                break;
+                            case "PB":
+                                slcUF = "498";
+                                break;
+                            case "PR":
+                                slcUF = "499";
+                                break;
+                            case "PE":
+                                slcUF = "500";
+                                break;
+                            case "PI":
+                                slcUF = "501";
+                                break;
+                            case "RJ":
+                                slcUF = "502";
+                                break;
+                            case "RN":
+                                slcUF = "503";
+                                break;
+                            case "RS":
+                                slcUF = "504";
+                                break;
+                            case "RO":
+                                slcUF = "505";
+                                break;
+                            case "RR":
+                                slcUF = "506";
+                                break;
+                            case "SC":
+                                slcUF = "507";
+                                break;
+                            case "SP":
+                                slcUF = "508";
+                                break;
+                            case "SE":
+                                slcUF = "509";
+                                break;
+                            case "TO":
+                                slcUF = "510";
+                                break;
+                            case "DF":
+                                slcUF = "511";
+                                break;
+                        }
+
                         if(registry.get(element.parentName + '.' + 'region_id')){
-                            registry.get(element.parentName + '.' + 'region_id').value(data.region_id ?? '');
+                            registry.get(element.parentName + '.' + 'region_id').value(slcUF ?? '');
                         }
                         if(registry.get(element.parentName + '.' + 'country_id')){
                             registry.get(element.parentName + '.' + 'country_id').value('BR');
                         }
+
+                        var deferred = jquery.Deferred();
+                        getPaymentInformationAction(deferred);
+                        jquery.when(deferred).done(function () {
+                            // isApplied(false);
+                            // totals.isLoading(false);
+                        });
                     }
                     jquery('body').loader('hide');
                 }).error(function(){
